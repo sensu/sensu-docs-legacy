cff2ece4d3390f6386376963ba2ca01799fe3482
Alan Smith <alan@asmith.me>
2015-03-03 16:18:38 -0500

    Added mouse-over header/title links

---
+++ source/js/header-rollover.js
@@ -0,0 +1,20 @@
+/*
+ * Puts a link icon at the end of header anchor tags
+ * when a user mouses over them. Useful for linking
+ * directly to doc subsections.
+ *
+ * Source: http://ben.balter.com/2014/03/13/pages-anchor-links/
+ * Date: 03/03/2015
+ */
+
+$(function() {
+  return $("h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="fa fa-link"></i>';
+    if (id) {
+      return $el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
