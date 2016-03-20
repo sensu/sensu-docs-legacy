bb28143c169d44fa0774f78c541381b9b3afa80d
Caleb Hailey <caleb@heavywater.io>
2016-03-19 17:29:46 -0700

    add alert markup support for all alert types

--- source/js/sensuapp.js
+++ source/js/sensuapp.js
@@ -1,10 +1,15 @@
 $(document).ready(function() {
-  var notes = $("em:contains('NOTE:')");
-  notes.addClass("note");
-  $.each(notes, function(index, note) {
-    var noteText = note.innerHTML;
-    note.innerHTML = noteText.replace("NOTE:", "<strong>NOTE:</strong>");
-  });
+
+  var alertStates = ["note", "success", "warning", "danger"];
+  for (i = 0; i < alertStates.length; i++) {
+    var alert = alertStates[i];
+    var alerts = $("em:contains(" + alert.toUpperCase() + ")")
+    $.each(alerts, function(index,alertHTML) {
+      var alertContent = alertHTML.innerHTML;
+      alertContent = alertContent.replace(alert.toUpperCase(), "<strong>" + alert.toUpperCase() + "</strong>");
+      alertHTML.innerHTML = "<div class='alert alert-" + alert + "'>" + alertContent + "</div>";
+    });
+  };
 
   $(":header:contains('Ubuntu/Debian')").addClass("ubuntu").prepend("<i class='fl-ubuntu'></i>");
   $(":header:contains('RHEL/CentOS')").addClass("centos").prepend("<i class='fl-centos'></i>");
